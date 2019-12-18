f143b4edafad4456e8bb3283bf4299190affb373
sa6kok <sa6kok@abv.bg>
2019-12-18 21:41:10 +0100

    some tests and corrections, constants

---
+++ src/main/java/com/localbandb/localbandb/web/view/constants/Constants.java
@@ -0,0 +1,33 @@
+package com.localbandb.localbandb.web.view.constants;
+
+public class Constants {
+   public final static String COULD_NOT_CHANGE_STATUS = "Could not change status!";
+    public final static String AVAILABLE_LOCATIONS_ALL_OVER_EUROPE= "Available locations all over Europe!";
+    public final static String MY_PROPERTIES= "My Properties";
+    public final static String PICTURE_WAS_NOT_ADDED = "\nPicture was not added!";
+    public final static String PAGE_TITLE= "My reservations";
+    public final static String RESERVATION_SUCCESSFULLY_CREATED = "Reservation was successfully created!";
+    public final static String PAYMENT_SUCCESSFUL = "Payment successful!";
+    public final static String PAYMENT_WAS_NOT_SUCCESSFUL_TRY_AGAIN = "Payment successful!";
+    public final static String RESERVATION_CANCELED_SUCCESSFUL = "You just canceled the reservation successful!";
+    public final static String RESERVATION_NOT_CANCELED_TRY_AGAIN = "Your try to cancel was not successful! Please try again!";
+    public final static String ADMIN = "ADMIN";
+    public final static String REVIEW_SUCCESSFULLY_ADDED = "Review was added successfully to the reservation!";
+    public final static String REVIEW_NOT_ADDED = "Something went wrong! Review was not added successfully to the reservation!";
+    public final static String AVAILABLE_LOCATION_IN = "Available locations in ";
+    public final static String SHOULD_BE_BETWEEN_3_15_SYMBOLS = "should be between 3 and 15 symbols!";
+    public final static String SHOULD_BE_AT_LEAST_15_SYMBOLS = "should be at least 15 symbols!";
+    public final static String SHOULD_BE_AT_LEAST_1 = " should be at least one!";
+    public final static String SHOULD_BE_A_POSITIVE_NUMBER = " should be a positive number!";
+    public final static String SHOULD_NOT_BE_EMPTY = " should not be empty!";
+    public final static String THE_DATE_IS_NOT_CORRECT = "The date is not correct";
+    public final static String SHOULD_BE_BETWEEN_3_25_SYMBOLS = "should be between 3 and 25 symbols!";
+    public final static String SHOULD_BE_BETWEEN_2_15_SYMBOLS = "should be between 2 and 15 symbols!";
+    public final static String NOT_VALID_USE_ONLY_LETTERS_OR_NUMBERS = " is not valid! Use only letters or numbers";
+    public final static String SHOULD_BE_AT_LEAST_SYMBOLS =  "should be at least 4 symbols!";
+    public final static String IS_NOT_VALID =  " is not valid!";
+    public final static String SHOULD_BE_AT_LEAST_18 =  " should be at least 18 years old!";
+
+
+
+}
